d35c0aa6d4b5e817a3226a814169f963b1d05786
jazzyMix <1072424611@qq.com>
2016-02-04 15:35:28 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,85 @@
+          <?php
+    if( $_GET["degree"] != null &&$_GET["address"] != null && $_GET["city"] != null && $_GET["state"] != "select" )
+    {
+$s = "https://maps.google.com/maps/api/geocode/xml?&address=".$_GET["address"].",".$_GET["city"].",".$_GET["state"]."&key=AIzaSyDemqBimkbHuYY3XLxzcpAxNpqUSTnksQI";
+       
+     $xml = simplexml_load_file($s);
+
+$forecast =  "https://api.forecast.io/forecast/4f7d198e171398f6cdf132b5d70fba22/"
+.$xml->result->geometry->location->lat.",".$xml->result->geometry->location->lng."?units=".$_GET["degree"]."&exclude=flags"; 
+
+        
+        
+  
+        
+        
+$json = json_decode(file_get_contents($forecast));
+        
+        
+        
+$timezone =  $json->timezone;
+       date_default_timezone_set($timezone) ;
+
+          
+            
+      
+            
+       $arr_8 = array();     
+        
+          for( $i = 0; $i< 8; $i ++){
+              
+
+        $sunriseTime1= $json->daily->data[$i]->sunriseTime;
+        
+$sunriseTime =new DateTime("@$sunriseTime1");
+        $sunriseTime->setTimezone(new DateTimeZone($timezone));
+$sunsetTime1 = $json->daily->data[$i]->sunsetTime;
+$sunsetTime = new DateTime("@$sunsetTime1");
+        $sunsetTime->setTimezone(new DateTimeZone($timezone));
+              array_push($arr_8,  $sunriseTime->format("h:i:A") ,  $sunsetTime->format("h:i:A") );
+
+
+
+              
+              
+
+              
+          }
+                  for( $i = 0; $i< 48; $i ++){
+              
+              $time= $json->hourly->data[$i]->time;
+
+        
+$time =new DateTime("@$time");
+        $time->setTimezone(new DateTimeZone($timezone));
+
+              array_push($arr_8,  $time->format("h:i:A"));
+
+
+
+              
+              
+
+              
+          }
+        
+        
+        
+        
+        
+        
+                
+
+      $json_arr = (array)$json;
+        
+                      array_unshift($json_arr,$arr_8);
+
+ $json = json_encode($json_arr); 
+
+        header('Content-type: application/x-javascript');
+
+        
+        
+ echo $json;  exit();
+    }
+?>
